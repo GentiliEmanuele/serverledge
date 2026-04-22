94256a24aabbd9e3a6ede73109d8571cc0da7390
emanuele <egentili2000@gmail.com>
2026-04-22 20:17:59 +0200

    Revert runtimes.go to original version

--- internal/container/runtimes.go
+++ internal/container/runtimes.go
@@ -2,14 +2,9 @@ package container
 
 // RuntimeInfo contains information about a supported function runtime env.
 type RuntimeInfo struct {
-	Image                Image
+	Image                string
 	InvocationCmd        []string
 	ConcurrencySupported bool
-}
-
-type Image struct {
-	LocalImage  string
-	RemoteImage string
 }
 
 const CUSTOM_RUNTIME = "custom"
@@ -18,17 +13,11 @@ var refreshedImages = map[string]bool{}
 
 var RuntimeToInfo = map[string]RuntimeInfo{
 	"python310": {
-		Image{
-			"localhost:5000/python310",
-			"grussorusso/serverledge-python310",
-		},
+		"grussorusso/serverledge-python310",
 		[]string{"python", "/entrypoint.py"},
 		true},
 	"nodejs17ng": {
-		Image{
-			"localhost:5000/nodejs17ng",
-			"grussorusso/serverledge-nodejs17ng",
-		},
+		"grussorusso/serverledge-nodejs17ng",
 		[]string{},
 		false},
 }
