2766617dbd55393f24af0933262c837329e47299
GentiliEmanuele <egentili2000@gmail.com>
2026-05-15 12:45:57 +0200

    Revert "Fix a bug in containers creation."
    This reverts commit 0e51603b3b92e9386f9aab57e41cf694912ca1c1.

--- internal/container/docker.go
+++ internal/container/docker.go
@@ -1,11 +1,14 @@
 package container
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
 	"log"
+	"net/http"
 	"strings"
 	"sync"
 	"time"
@@ -39,9 +42,10 @@ func InitDockerContainerFactory() *DockerFactory {
 }
 
 func (cf *DockerFactory) Create(image string, opts *ContainerOptions) (ContainerID, error) {
+	var img string
 
 	if !cf.HasImage(image) {
-		image, _ = cf.PullImage(image)
+		img, _ = cf.PullImage(image)
 		// error ignored, as we might still have a stale copy of the image
 	}
 
@@ -52,7 +56,7 @@ func (cf *DockerFactory) Create(image string, opts *ContainerOptions) (Container
 	}
 
 	resp, err := cf.cli.ContainerCreate(cf.ctx, &container.Config{
-		Image: image,
+		Image: img,
 		Cmd:   opts.Cmd,
 		Env:   opts.Env,
 		Tty:   false,
@@ -153,7 +157,46 @@ func (cf *DockerFactory) PullImage(img string) (string, error) {
 	log.Printf("Pulled image: %s\n", img)
 	refreshedImages[img] = true
 
+	// img != localImage means that the image was pulled from Docker Hub
+	if img != localImage {
+		// If the image was pulled from Docker hub require to the registry to pull the image
+		go func() {
+			err = pullRequest(localRegistryAddress, img)
+			if err != nil {
+				log.Printf("Error while communicating with local registry: %v\n", err)
+			}
+		}()
+	}
+
 	return img, nil
+}
+
+func pullRequest(localRegistryAddress, img string) error {
+	// Format the URL using to invoke the API
+	url := fmt.Sprintf("http://%s/pull-image", localRegistryAddress)
+
+	// Define the payload as a map
+	payload := map[string]string{"image": img}
+
+	// Serialize data in json format
+	jsonData, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		return err
+	}
+
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Printf("Could not close http response\n")
+		}
+	}(resp.Body)
+
+	return nil
 }
 
 func getLocalRegistryAddress() (string, error) {
