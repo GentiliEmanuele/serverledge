85e8b585f2e868f3c0e0e6fa80615f20f18bb7db
GentiliEmanuele <egentili2000@gmail.com>
2026-08-23 14:29:21 +0200

    Fixed some bugs

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -72,6 +72,7 @@ func (c *cache) Set(k string, x interface{}, d time.Duration) {
 		c.items[k] = &newItem
 	} else {
 		toReplace := c.findLRU() //find least recently used key
+		c.onEvicted(toReplace, c.items[toReplace].Object)
 		delete(c.items, toReplace)
 		c.items[k] = &newItem //insert a new item
 	}
@@ -110,6 +111,7 @@ func (c *cache) Get(k string) (interface{}, bool) {
 		now := time.Now().UnixNano()
 		if now > item.Expiration {
 			c.mu.RUnlock()
+			c.onEvicted(k, item.Object)
 			return nil, false
 		}
 	}
