7861aa17a35ee9343f2d60740fb6d62c62b733a4
GentiliEmanuele <egentili2000@gmail.com>
2026-09-21 21:18:34 +0200

    Modified a function's example

--- examples/benchmarks/model/use.py
+++ examples/benchmarks/model/use.py
@@ -8,8 +8,8 @@ def handler(params, context):
     weights_path = os.path.join(base_dir, 'my.weights.h5')
 
     # Recreate the model without weight
-    model = tf.keras.applications.VGG16(weights=None, input_shape=(224, 224, 3))
-    #model = tf.keras.applications.MobileNetV2(weights=None, input_shape=(224, 224, 3))
+    #model = tf.keras.applications.VGG16(weights=None, input_shape=(224, 224, 3))
+    model = tf.keras.applications.MobileNetV2(weights=None, include_top=False, alpha=0.35)
 
     # 2.Load the weights
     model.load_weights(weights_path)
